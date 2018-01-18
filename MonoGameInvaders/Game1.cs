3bf1dd71530ffbb6538dbd153b7ebcdf82fb7b83
Bas Pijls <b.pijls@hva.nl>
2018-01-18 10:58:45 +0100

    Initial Commit

---
+++ MonoGameInvaders/Game1.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameInvaders
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D background, scanlines;
+
+        Player thePlayer;
+        Bullet theBullet;
+
+        //TODO: Add multiple invaders here
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);            
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.ApplyChanges();
+
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // Pass often referenced variables to Global
+            Global.GraphicsDevice = GraphicsDevice;            
+            Global.content = Content;
+
+            // Create and Initialize game objects
+            thePlayer = new Player();
+            theBullet = new Bullet();
+
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Global.spriteBatch = spriteBatch;
+            background = Content.Load<Texture2D>("spr_background");
+            scanlines = Content.Load<Texture2D>("spr_scanlines");
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Pass keyboard state to Global so we can use it everywhere
+            Global.keys = Keyboard.GetState();
+            if (Global.keys.IsKeyDown(Keys.Space)) theBullet.Fire(thePlayer.position);
+            // Update the game objects
+            thePlayer.Update();
+            theBullet.Update();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {            
+            spriteBatch.Begin();
+            // Draw the background (and clear the screen)
+            spriteBatch.Draw(background, Global.screenRect, Color.White);
+
+            // Draw the game objects
+            thePlayer.Draw();
+            theBullet.Draw();
+
+            spriteBatch.Draw(scanlines, Global.screenRect, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MonoGameInvaders/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameInvaders
+{
+    class Player
+    {
+        public Vector2 position;
+        public Vector2 velocity;
+        public Texture2D texture;
+      
+        public Player()
+        {
+            texture = Global.content.Load<Texture2D>("spr_ship");
+            Init();       
+        }
+
+        public void Init()
+        {
+            position.X = Global.width / 2; // horizontal center on screen
+            position.Y = Global.height - texture.Height; // bottom of screen
+        }
+
+        public bool Update()
+        {
+            // Assume player is not moving
+            velocity.X = 0;
+
+            // Alter velocity when keys are pressed
+            if (Global.keys.IsKeyDown(Keys.Left)) velocity.X = -10.0f;
+            if (Global.keys.IsKeyDown(Keys.Right)) velocity.X = 10.0f;           
+
+            position += velocity;
+
+            // If x position is out of bounds, "undo" velocity
+            if ((position.X < 100) || (position.X > Global.width - texture.Width - 100))
+                position -= velocity;
+          
+            return true;
+        }
+
+        public void Draw()
+        {
+            Global.spriteBatch.Draw(texture, position, Color.White);
+        }
+
+       
+    }
+}
